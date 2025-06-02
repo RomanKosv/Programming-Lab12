38ba104f57449f3e74f1a82030a038c3037c0d62
RomanKosv <romkos2006@gmail.com>
2025-06-02 15:30:01 +0500

    Restore tests

--- RunProject/LinkList.cs
+++ RunProject/LinkList.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-public class LinkList<T> : IEnumerable<T>, IList<T>
+public class LinkList<T> : IEnumerable<T>//, IList<T>
 {
     Node? start;
 
